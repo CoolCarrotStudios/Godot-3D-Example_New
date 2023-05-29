4ea2b55c4a59a223196a7b48c3193ab8c8900cd3
Cool Carrot Studios <>
2023-05-29 23:47:21 +0100

    Player Complete

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody3D
+
+@export var speed = 14
+
+@export var fall_acceleration = 75
+
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_up"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.z += 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():	
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	velocity = target_velocity
+	move_and_slide()
